b911a08ee7be7b1d03e92cd6fedc8da905ab0c86
Felipe P. Capovilla <felipe.pavanello.capovilla@gmail.com>
2026-08-20 15:41:40 -0300

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> hash;
+
+        for(int k=0; k<nums.size();k++){
+            hash[nums[k]]++;
+        }
+
+
+        int maxSize =0;
+
+        for(int i=0;i<nums.size();i++){
+            int cont=0;
+
+            if(!hash.count(nums[i]-1)){
+                int k =nums[i];
+
+                while(hash.count(k)){
+                    cont++;
+                    k+=1;
+                }
+
+                maxSize = max(maxSize,cont);
+            }
+        }
+
+
+        return maxSize;
+
+
+        
+    }
+};
