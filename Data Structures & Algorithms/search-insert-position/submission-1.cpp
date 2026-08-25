9a8e148fade769692ff3f1be54e5ff9e9e3649ba
Felipe P. Capovilla <felipe.pavanello.capovilla@gmail.com>
2026-08-25 13:12:48 -0300

    Add: search-insert-position - submission-1

---
+++ Data Structures & Algorithms/search-insert-position/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int r,l,mid;
+        l = 0;
+        r = nums.size()-1;
+
+        while(l <= r){
+            mid = (l+r)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target > nums[mid]){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+
+        return l;
+        
+    }
+};
