92f42fb3cf80a6ad1aa5e7ea4b6346599d5f10cd
Felipe P. Capovilla <felipe.pavanello.capovilla@gmail.com>
2026-09-04 13:16:35 -0300

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int r=nums.size()-1;
+        int l=0;
+
+        while(r >= l){
+            int mid = (r+l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+
+        return -1;
+    }
+};
