b1b20acea93da3031bde9c3fc634c8aa9628a405
Felipe P. Capovilla <felipe.pavanello.capovilla@gmail.com>
2026-09-08 16:10:05 -0300

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        int n = s.size();
+        if(n < 2){
+            return n;
+        }
+
+        unordered_set<char> caracteres;
+
+        for(int i=0;i<n;i++){
+            caracteres.insert(s[i]);
+        }
+
+        int tam=0;
+        for(const char& c : caracteres){
+            int r=0;
+            int l=0;
+            int diff=0;
+            while(r < n){
+                if(s[r] != c){
+                    diff++;
+                }
+
+                while(diff > k){
+                    if(s[l] != c){
+                        diff--;
+                    }
+                    l++;
+                }
+
+                tam = max(tam,r-l+1);
+                r++;
+            }
+        }
+
+        return tam;
+        
+    }
+};
